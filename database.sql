691c6b0737be93bc39532fc586e029a4eaec21e3
Joshua Duenow <joshjduenow@gmail.com>
2023-11-22 10:17:36 -0600

    create database and servers working

--- database.sql
+++ database.sql
@@ -1,2 +1,15 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+-- DROP TABLE IF EXISTS "shopping";
+
+-- CREATE TABLE "shopping" (
+-- 	"id" SERIAL PRIMARY KEY,
+-- 	"name" VARCHAR(80) NOT NULL,
+-- 	"quantity" INTEGER,
+-- 	"unit" VARCHAR(20) NOT NULL
+-- );
+
+-- INSERT INTO "shopping"
+--   ("name", "quantity", "unit")
+--   VALUES 
+--   ('Mac & Cheese', 1, 'Box');
