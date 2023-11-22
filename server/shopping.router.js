a33f9415145dee9c56e3e641b713818de58fc721
Joshua Duenow <joshjduenow@gmail.com>
2023-11-22 11:23:16 -0600

    fixed get route problem in app

---
+++ server/shopping.router.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('./modules/pool.js'); // Might mess up
+
+// Setup a GET route to get all the guest from the database
+router.get('/', (req, res) => {
+    // When you fetch all things in these GET routes, strongly encourage ORDER BY
+    // so that things always come back in a consistent order 
+    const sqlText = `SELECT * FROM "shopping" ORDER BY "id";`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log('Got stuff back from the database', result.rows);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
+
+
+// Setup a POST route to add a new guest to the database
+router.post('/', (req, res) => {
+    const list = req.body;
+    const sqlText = `INSERT INTO shopping ("name", "quantity", "unit")
+                     VALUES ($1, $2, $3)`;
+    // Let sql sanitize your inputs (NO Bobby Drop Tables here!)
+    // the $1, $2, etc get substituted with the values from the array below
+    pool.query(sqlText, [list.name, list.quantity, list.unit])
+        .then((result) => {
+            console.log(`Added shopping item to the database`, list);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
+
+
+module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
-
-import Header from '../Header/Header.jsx'
-import './App.css';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import Header from "../Header/Header.jsx";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  let [shoppingList, setShoppingList] = useState([]);
+
+  useEffect(() => {
+    getShoppingList();
+  }, []); // <---- Stop Sign
+
+  const getShoppingList = () => {
+    axios
+      .get("/shopping")
+      .then((response) => {
+        setShoppingList(response.data);
+      })
+      .catch((err) => {
+        alert("error getting shopping list");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+      </main>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shoppingList.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.unit}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
